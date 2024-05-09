c9fb761f9bcdd1d83adacefcf81dccc3d5a91a6d
Divyansh kumar <divyanshkumar@Divyanshs-MacBook-Air.local>
2024-05-10 02:51:25 +0530

    main

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5001/api/v1";
+axios.defaults.baseURL =
+  "https://mern-ai-chatbot-backend-mdcm88bav-divyansh-kumar-s-projects.vercel.app/api/v1";
 axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
